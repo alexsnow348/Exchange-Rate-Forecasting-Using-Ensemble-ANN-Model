2894ca74b956200439edf8a43a98314845911ae0
wuthmone <whlaing10@gmail.com>
2017-01-11 11:22:36 +0800

    Added new files

--- Data_Processing.R
+++ Data_Processing.R
@@ -1,45 +1,104 @@
-
 ## Read  USD dataset and process the dataset
+
+## Function Parameters
+## Datasource_url: "/home/wut/Desktop/Link to Data/FYP Program/Raw Data/alldata.csv"
+## Predictor order : 1-10
+## Currency : "U.S. Dollar" / "Australian Dolla"/ "Canadian Dollar"/"Euro"
+##            "Pound Sterling"/"Singapore Dollar"/"Swiss Franc"
+
+## Return Outcomes
+## Date for predicted day, Test Dataset, Train Dataset
+
+
 #Training variables
-alldata = tbl_df(read.csv("alldata.csv"))
-usd = filter(alldata, currency=="U.S. Dollar")
-newdata = tbl_df(usd)
-usd_df = select(newdata, Date, Value)
-usd_non_normalize = select(usd_df, Value)
-usd_value = as.tbl(normalized(usd_non_normalize))
-names(usd_value) = "USD"
-result =  createTimeSlices(usd_value$USD, 3, 1, fixedWindow = T)
-train_data = training_data(result, usd_value)
-vector_train = as.vector(train_data$USD,"any")
-matrix_train <- matrix(vector_train, nrow = 3 , ncol = length(vector_train)/3)
-
-
-#Predicted Date
-Date = createTimeSlices(usd$Date, 3, 1, fixedWindow = T)
-test_date = testing_data(Date,usd)
-
-
-#Class Variable
-test_data =  testing_data(result,usd_value)
-actual_date_and_value = select(usd, Date, Value)
-
-#input_train 60 % && output_train 60 %
-training_input = matrix_train[1:3, 1:701]
-training_output = test_data[1:701,]
-
-new_input = t(training_input)
-train_input = as.data.frame(new_input)
-train_dataset <- cbind(train_input, training_output)
-names(train_dataset) <- c("firstDay","secondDay", "thirdDay", "oneDayAhead")
-
-#input_test 40% && output_test 40%
-testing_input = matrix_train[1:3, 702:1168]
-testing_output = test_data[702:1168,]
-
-new_input = t(testing_input)
-test_input = as.data.frame(new_input)
-test_dataset = cbind(test_input, testing_output)
-names(test_dataset) <- c("firstDay","secondDay", "thirdDay", "oneDayAhead")
-
-
-
+Data_Processing <- function(datasource_url, predictor_order, ex_currency){ 
+                                                                                
+        alldata = tbl_df(read.csv(datasource_url))
+        usd = filter(alldata, currency ==ex_currency)
+        newdata = tbl_df(usd)
+        usd_df = select(newdata, Date, Value)
+        usd_non_normalize = select(usd_df, Value)
+        usd_value = as.tbl(normalized(usd_non_normalize))
+        names(usd_value) = "USD"
+        result =  createTimeSlices(usd_value$USD, predictor_order, 1, fixedWindow = T)
+        train_data = training_data(result, usd_value)
+        vector_train = as.vector(train_data$USD,"any")
+        matrix_train <- matrix(vector_train, nrow = predictor_order, ncol = length(vector_train)/predictor_order)
+      
+        #Class Variable
+        test_data =  testing_data(result,usd_value)
+       
+        
+        #input_train 60 % && output_train 60 %
+        train_until = ceiling(length(matrix_train)*0.6/predictor_order)
+        training_input = matrix_train[1:predictor_order, 1:train_until]
+        train_end = ceiling(length(test_data$USD)*0.6)
+        training_output = test_data[1:train_end,]
+        
+        new_input = t(training_input)
+        train_input = as.data.frame(new_input)
+        train_dataset <- cbind(train_input, training_output)
+        
+        
+        #input_test 40% && output_test 40%
+        test_until <- train_until+1
+        test_end <- train_end+1
+        matrix_end <- length(matrix_train)/predictor_order
+        testing_input = matrix_train[1:predictor_order, test_until: matrix_end]
+        testing_output = test_data[test_end:length(test_data$USD),]
+        
+        new_input = t(testing_input)
+        test_input = as.data.frame(new_input)
+        test_dataset = cbind(test_input, testing_output)
+        
+        #Nameing columns
+        if (predictor_order==3){
+                names(train_dataset) <- c("firstDay","secondDay", "thirdDay", "oneDayAhead") 
+                names(test_dataset) <- c("firstDay","secondDay", "thirdDay", "oneDayAhead")
+        }
+        
+        if(predictor_order==4){
+                names(train_dataset) <- c("firstDay","secondDay", "thirdDay","fourthDay", "oneDayAhead") 
+                names(test_dataset) <- c("firstDay","secondDay", "thirdDay", "fourthDay","oneDayAhead")
+        }
+        
+        if(predictor_order==5){
+                names(train_dataset) <- c("firstDay","secondDay", "thirdDay","fourthDay","FifthDay", "oneDayAhead") 
+                names(test_dataset) <- c("firstDay","secondDay", "thirdDay", "fourthDay","Fifthday","oneDayAhead")
+        }
+        
+        if(predictor_order==6){
+                names(train_dataset) <- c("firstDay","secondDay", "thirdDay","fourthDay","FifthDay","SixthDay", "oneDayAhead") 
+                names(test_dataset) <- c("firstDay","secondDay", "thirdDay", "fourthDay","Fifthday","SixthDay","oneDayAhead")
+        }
+        
+        if(predictor_order==7){
+                names(train_dataset) <- c("firstDay","secondDay", "thirdDay","fourthDay","FifthDay","SixthDay","SeventhDay","oneDayAhead") 
+                names(test_dataset) <- c("firstDay","secondDay", "thirdDay", "fourthDay","Fifthday","SixthDay","SeventhDay","oneDayAhead")
+        }
+        
+        if(predictor_order==8){
+                names(train_dataset) <- c("firstDay","secondDay", "thirdDay","fourthDay","FifthDay","SixthDay","SeventhDay","eighthDay","oneDayAhead") 
+                names(test_dataset) <- c("firstDay","secondDay", "thirdDay", "fourthDay","Fifthday","SixthDay","SeventhDay","eighthDay","oneDayAhead")
+        }
+        if(predictor_order==9){
+                names(train_dataset) <- c("firstDay","secondDay", "thirdDay","fourthDay","FifthDay","SixthDay","SeventhDay","eighthDay","ninethDay","oneDayAhead") 
+                names(test_dataset) <- c("firstDay","secondDay", "thirdDay", "fourthDay","Fifthday","SixthDay","SeventhDay","eighthDay","ninethDay","oneDayAhead")
+        }
+        if(predictor_order==10){
+                names(train_dataset) <- c("firstDay","secondDay", "thirdDay","fourthDay","FifthDay","SixthDay","SeventhDay","eighthDay","ninethDay","tenthDay","oneDayAhead") 
+                names(test_dataset) <- c("firstDay","secondDay", "thirdDay", "fourthDay","Fifthday","SixthDay","SeventhDay","eighthDay","ninethDay","tenthDay","oneDayAhead")
+        }
+        
+        
+        
+        #Predicted Date
+        Date = createTimeSlices(usd$Date, predictor_order, 1, fixedWindow = T)
+        date = testing_data(Date,usd)
+        test_date = as.data.frame(date[test_end:length(test_data$USD),]$Date)
+        names(test_date)= "Date"
+        
+        output <- list(train_dataset,test_dataset,test_date,usd_non_normalize)
+        
+        
+}
---
+++ main.R
@@ -0,0 +1,72 @@
+
+#Main Modeling & Training and Validation
+
+source("requried_functions_N_libraries.R")
+source("Data_Processing.R")
+dataset<- Data_Processing("/home/wut/Desktop/Link to Data/FYP Program/Raw Data/alldata.csv",3,"Euro")
+train_dataset <- dataset[[1]]
+test_dataset <- dataset[[2]]
+date <- dataset[[3]]
+usd_non_normalize <- dataset[[4]]
+
+## HOMOGENEOUS MODEL
+## Train the network using neuralnet (First MLP)
+exchange_model <- neuralnet(oneDayAhead ~ firstDay + secondDay + thirdDay,
+                            data = train_dataset, hidden = 2)
+
+model_results <- neuralnet::compute(exchange_model, test_dataset[1:3])
+predicted_oneDayhead <- model_results$net.result
+predict_value <- as.data.frame(denormalized(predicted_oneDayhead))
+actual <- denormalized(test_dataset[,4])
+
+## Second MLP
+exchange_model2 <- neuralnet(oneDayAhead ~ firstDay + secondDay + thirdDay,
+                            data = train_dataset, hidden = 2)
+
+model_results2 <- neuralnet::compute(exchange_model2, test_dataset[1:3])
+predicted_oneDayhead2 <- model_results2$net.result
+predict_value2 <- as.data.frame(denormalized(predicted_oneDayhead2))
+
+## Third MLP
+exchange_model3 <- neuralnet(oneDayAhead ~ firstDay + secondDay + thirdDay,
+                            data = train_dataset, hidden = 2)
+
+model_results3 <- neuralnet::compute(exchange_model3, test_dataset[1:3])
+predicted_oneDayhead3 <- model_results3$net.result
+predict_value3 <- as.data.frame(denormalized(predicted_oneDayhead3))
+
+all_predicted <- c(predict_value,predict_value2,predict_value3)
+all_predicted <-as.data.frame(all_predicted)
+names(all_predicted)<- c("First MLP", "Second MLP", "Third MLP")
+
+lapply(list, function)
+## Denormalizeing process and error calculation (NeuralNet with 1 hidden node)
+predict_value <- as.vector(denormalized(predicted_oneDayhead))
+actual <- denormalized(test_dataset[,4])
+error <- actual - predict_value
+# Example of invocation of functions
+rmse(error) 
+mae(error) 
+
+
+
+## Train using nnet using two hidden node
+fitnn = nnet(oneDayAhead~firstDay+secondDay+thirdDay, train_dataset, size=2)
+test_result <- predict(fitnn, test_dataset[1:3])
+predict_value <- as.vector(denormalized(test_result))
+actual <- denormalized(test_dataset[,4])
+error <- actual - predict_value
+
+# Example of invocation of functions
+rmse(error) 
+mae(error) 
+
+
+## Final reasult
+
+data_result <- cbind(test_date[702:1168,5],actual,predict_value$V1,error)
+names(data_result) <- c("Date","Actual_Value","Predicted_Value", "Error")
+
+# Writing to xlsx file
+library(xlsx)
+write.xlsx(data_result, "data_result.xlsx")
